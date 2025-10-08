a0806558ad8a9d7203dc0af985d97a12b993613c
Juan Manuel Gelay <juanmanuelgelay@gmail.com>
2025-10-08 17:28:59 -0300

    creating and preparing the application

--- src/main.js
+++ src/main.js
@@ -1,1 +1,17 @@
-( async () => {} )();
+import { Application } from 'pixi.js';
+
+( async () => {
+    const app = new Application();
+    await app.init({
+        resizeTo: window,
+        
+        // Set the background color to yellow and the alpha value
+        backgroundAlpha: 0.4,
+        backgroundColor: 0xffea00
+    });
+    
+    // Delete the margin around the canvas
+    app.canvas.style.position = 'absolute';
+
+    document.body.appendChild(app.canvas);
+} )();
